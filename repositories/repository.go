19e55114ff973085bfa4019ac73153ac57d5d28e
eroatta <emilianoroatta@gmail.com>
2018-11-16 19:05:58 -0300

    added some funcitons to the cloner interface

--- repositories/repository.go
+++ repositories/repository.go
@@ -2,21 +2,21 @@ package repositories
 
 import (
 	"log"
+	"os"
 
 	"gopkg.in/src-d/go-billy.v4/memfs"
 	git "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/storage/memory"
 )
 
-//func Clone(cloner Cloner, url string) (*Repository, error) {
-//
-//}
-
+// Cloner provides the required functions to clone, get the list of files and folders that are part of, and
+// retrieve a file from a repository.
 type Cloner interface {
 	Clone(url string) (*Repository, error)
-	GetFiles(repo string)
+	GetFiles(repo string) ([]os.FileInfo, error)
 }
 
+// DefaultGithubCloner builds a component that can access a GitHub repository, using the default configuration and client.
 func DefaultGithubCloner() Cloner {
 	return GoGitCloner{}
 }
@@ -26,6 +26,10 @@ type GoGitCloner struct {
 
 func (c GoGitCloner) Clone(url string) (*Repository, error) {
 	return nil, nil
+}
+
+func (c GoGitCloner) GetFiles(repo string) ([]os.FileInfo, error) {
+	return make([]os.FileInfo, 0), nil
 }
 
 // Clone clones a GitHub repository, and return a struct that represents it.
