91ecfec3b9d576ada1544ba62e2bf58c16a0948c
eroatta <emilianoroatta@gmail.com>
2018-11-22 09:04:09 -0200

    defined test cases for cloner implementation

--- repositories/cloner_test.go
+++ repositories/cloner_test.go
@@ -14,3 +14,35 @@ func TestDefaultGithubCloner_ShouldReturnNewCloner(t *testing.T) {
 func TestClone_GithubCloner_ShouldCloneTheRepository(t *testing.T) {
 
 }
+
+func TestClone_GithubClonerRepoNotFound_ShouldReturnAnError(t *testing.T) {
+
+}
+
+func TestGetFilesInfo_GithubClonerWith5Files_ShouldReturnAnArrayOfFileInfoWith5Elements(t *testing.T) {
+
+}
+
+func TestGetFilesInfo_GithubClonerWith2Files1Folder_ShouldReturnAnArrayOfFileInfoWith2Elements(t *testing.T) {
+
+}
+
+func TestGetFilesInfo_GithubClonerNoFiles_ShouldReturnAnEmptyArray(t *testing.T) {
+
+}
+
+func TestGetFilesInfo_GithubClonerError_ShouldReturnAnError(t *testing.T) {
+
+}
+
+func TestGetFile_GithubClonerEmptyFilename_ShouldReturnAnEmptyArray(t *testing.T) {
+
+}
+
+func TestGetFile_GithubClonerExistingFile_ShouldReturnAnArrayOfBytes(t *testing.T) {
+
+}
+
+func TestGetFile_GithubClonerNoFile_ShouldReturnAnError(t *testing.T) {
+
+}
