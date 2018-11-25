b5379e419343b029afc8ac018c8ea67aba8b130e
eroatta <emilianoroatta@gmail.com>
2018-11-25 16:02:46 -0300

    simplified design

--- repositories/repository.go
+++ repositories/repository.go
@@ -1,30 +1,43 @@
 package repositories
 
 import (
+	"errors"
+	"log"
 	"os"
 
+	"gopkg.in/src-d/go-billy.v4/memfs"
 	git "gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/storage/memory"
 )
 
-// Repository represents a remote repository.
-type Repository interface {
-	GetFilesInfo() ([]os.FileInfo, error)
-	GetFile(filename string) ([]byte, error)
+// ClonerFunc defines the interface for cloning a remote Git repository.
+type ClonerFunc func(url string) (*git.Repository, error)
+
+// Clone takes a repository URL (https://github.com/USER/REPO or git@github.com:/USER/REPO) and
+// clones it, using the provided cloner.
+func Clone(cloner ClonerFunc, url string) (*git.Repository, error) {
+	log.Println("Cloning repository...")
+	repository, err := cloner(url)
+	if err != nil {
+		return nil, errors.New("Error cloning remote repository")
+	}
+
+	return repository, nil
 }
 
-// GoGitRepository represents a remote GitHub repository.
-type GoGitRepository struct {
-	//Repository
-
-	repository *git.Repository
+// GoGitClonerFunc clones a remote GitHub repository using the src{d}/go-git client.
+func GoGitClonerFunc(url string) (*git.Repository, error) {
+	return git.Clone(memory.NewStorage(), memfs.New(), &git.CloneOptions{
+		URL: url,
+	})
 }
 
-// GetFilesInfo retrieves the list of files on a given repository.
-func (r GoGitRepository) GetFilesInfo() ([]os.FileInfo, error) {
+// FilesInfo retrieves the list of files and its related information on a given repository.
+func FilesInfo(repository *git.Repository) ([]os.FileInfo, error) {
 	return make([]os.FileInfo, 0), nil
 }
 
-// GetFile retrieves the raw file as an array of bytes.
-func (r GoGitRepository) GetFile(filename string) ([]byte, error) {
+// File retrieves the raw file as an array of bytes.
+func File(repository *git.Repository, filename string) ([]byte, error) {
 	return make([]byte, 0), nil
 }
--- repositories/repository_test.go
+++ repositories/repository_test.go
@@ -4,22 +4,23 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	git "gopkg.in/src-d/go-git.v4"
 )
 
-func TestReadDirAndSubDirs(t *testing.T) {
-	//fs := memfs.New()
-
-	//files, err := GetFiles()
-	//assert.Equal(t, 4, len(files), "should match number of files")
+func TestClone_ClonerOK_ShouldReturnRepository(t *testing.T) {
+	assert.Fail(t, "unimplemented test")
+}
+func TestClone_ClonerError_ShouldReturnAnError(t *testing.T) {
+	assert.Fail(t, "unimplemented test")
 }
 
-func TestGetFilesInfo_GoGitRepositoryWith5Files_ShouldReturnAnArrayOfFileInfoWith5Elements(t *testing.T) {
+func TestFilesInfo_RepositoryWith5Files_ShouldReturnAnArrayOfFileInfoWith5Elements(t *testing.T) {
 
 	expectedFilenames := []string{"main.go", "file.go", "file_test.go", "README.md", "license.txt"}
 
-	repository := GoGitRepository{}
+	repository := git.Repository{}
 
-	files, err := repository.GetFilesInfo()
+	files, err := FilesInfo(&repository)
 	if err != nil {
 		assert.Fail(t, "shouldn't get an error while retrieving the list of files")
 	}
@@ -33,26 +34,26 @@ func TestGetFilesInfo_GoGitRepositoryWith5Files_ShouldReturnAnArrayOfFileInfoWit
 	assert.ElementsMatch(t, expectedFilenames, filenames, "filenames don't match")
 }
 
-func TestGetFilesInfo_GoGitRepositoryWith2Files1Folder_ShouldReturnAnArrayOfFileInfoWith2Elements(t *testing.T) {
+func TestFilesInfo_RepositoryWith2Files1Folder_ShouldReturnAnArrayOfFileInfoWith2Elements(t *testing.T) {
 	assert.Fail(t, "unimplemented test")
 }
 
-func TestGetFilesInfo_GoGitRepositoryNoFiles_ShouldReturnAnEmptyArray(t *testing.T) {
+func TestFilesInfo_RepositoryNoFiles_ShouldReturnAnEmptyArray(t *testing.T) {
 	assert.Fail(t, "unimplemented test")
 }
 
-func TestGetFilesInfo_GoGitRepositoryError_ShouldReturnAnError(t *testing.T) {
+func TestFilesInfo_RepositoryError_ShouldReturnAnError(t *testing.T) {
 	assert.Fail(t, "unimplemented test")
 }
 
-func TestGetFile_GoGitRepositoryEmptyFilename_ShouldReturnAnEmptyArray(t *testing.T) {
+func TestFile_RepositoryEmptyFilename_ShouldReturnAnEmptyArrayOfBytes(t *testing.T) {
 	assert.Fail(t, "unimplemented test")
 }
 
-func TestGetFile_GoGitRepositoryExistingFile_ShouldReturnAnArrayOfBytes(t *testing.T) {
+func TestFile_RepositoryExistingFile_ShouldReturnAnArrayOfBytes(t *testing.T) {
 	assert.Fail(t, "unimplemented test")
 }
 
-func TestGetFile_GoGitRepositoryNoFile_ShouldReturnAnError(t *testing.T) {
+func TestFile_RepositoryNoFile_ShouldReturnAnError(t *testing.T) {
 	assert.Fail(t, "unimplemented test")
 }
