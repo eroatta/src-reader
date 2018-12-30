f0cf62fe8c7dec1308bc4d931902e63728e59710
eroatta <emilianoroatta@gmail.com>
2018-12-30 16:49:33 -0300

    added support for var and const declaration on the samurai mining process

--- extractors/extractor_test.go
+++ extractors/extractor_test.go
@@ -66,3 +66,112 @@ func TestVisit_OnSamuraiWithVarDeclNode_ShouldSplitTheName(t *testing.T) {
 	assert.Equal(t, 1, extractor.words["test"])
 	assert.Equal(t, 1, extractor.words["iden"])
 }
+
+func TestVisit_OnSamuraiWithVarDeclAndMultipleNames_ShouldSplitAllTheNames(t *testing.T) {
+	samurai := NewSamuraiExtractor()
+
+	fs := token.NewFileSet()
+	var src = `
+		package main
+		var testIden, testBar string
+	`
+
+	node, _ := parser.ParseFile(fs, "", []byte(src), parser.AllErrors)
+	//ast.Print(fs, node)
+	ast.Walk(samurai, node)
+
+	assert.NotNil(t, samurai)
+
+	extractor := samurai.(SamuraiExtractor)
+	assert.NotEmpty(t, extractor.words)
+	assert.Equal(t, 2, extractor.words["test"])
+	assert.Equal(t, 1, extractor.words["iden"])
+	assert.Equal(t, 1, extractor.words["bar"])
+}
+
+func TestVisit_OnSamuraiWithConstDeclNode_ShouldSplitTheName(t *testing.T) {
+	samurai := NewSamuraiExtractor()
+
+	fs := token.NewFileSet()
+	var src = `
+		package main
+		const testIden = "boo"
+	`
+
+	node, _ := parser.ParseFile(fs, "", []byte(src), parser.AllErrors)
+	//ast.Print(fs, node)
+	ast.Walk(samurai, node)
+
+	assert.NotNil(t, samurai)
+
+	extractor := samurai.(SamuraiExtractor)
+	assert.NotEmpty(t, extractor.words)
+	assert.Equal(t, 1, extractor.words["test"])
+	assert.Equal(t, 1, extractor.words["iden"])
+	assert.Equal(t, 1, extractor.words["boo"])
+}
+
+func TestVisit_OnSamuraiWithConstDeclAndMultipleNamesAndValues_ShouldSplitAllTheNames(t *testing.T) {
+	samurai := NewSamuraiExtractor()
+
+	fs := token.NewFileSet()
+	var src = `
+		package main
+		const testIden, foo = "boo", "bar"
+	`
+
+	node, _ := parser.ParseFile(fs, "", []byte(src), parser.AllErrors)
+	//ast.Print(fs, node)
+	ast.Walk(samurai, node)
+
+	assert.NotNil(t, samurai)
+
+	extractor := samurai.(SamuraiExtractor)
+	assert.NotEmpty(t, extractor.words)
+	assert.Equal(t, 1, extractor.words["test"])
+	assert.Equal(t, 1, extractor.words["iden"])
+	assert.Equal(t, 1, extractor.words["boo"])
+	assert.Equal(t, 1, extractor.words["foo"])
+	assert.Equal(t, 1, extractor.words["bar"])
+}
+
+func TestVisit_OnSamuraiWithConstDeclBlock_ShouldSplitAllTheNames(t *testing.T) {
+	samurai := NewSamuraiExtractor()
+
+	fs := token.NewFileSet()
+	var src = `
+		package main
+		const (
+			testIden = "boo"
+			foo = "bar"
+		)
+	`
+
+	node, _ := parser.ParseFile(fs, "", []byte(src), parser.AllErrors)
+	//ast.Print(fs, node)
+	ast.Walk(samurai, node)
+
+	assert.NotNil(t, samurai)
+
+	extractor := samurai.(SamuraiExtractor)
+	assert.NotEmpty(t, extractor.words)
+	assert.Equal(t, 1, extractor.words["test"])
+	assert.Equal(t, 1, extractor.words["iden"])
+	assert.Equal(t, 1, extractor.words["boo"])
+	assert.Equal(t, 1, extractor.words["foo"])
+	assert.Equal(t, 1, extractor.words["bar"])
+}
+
+func TestVisit_OnSamuraiWithTypeDeclNode_ShouldSplitTheName(t *testing.T) {
+	assert.Fail(t, "unimplemented test")
+}
+
+func TestVisit_OnSamuraiWithTypeDeclNodeAndMultipleNames_ShouldSplitAllTheNames(t *testing.T) {
+	assert.Fail(t, "unimplemented test")
+}
+
+func TestVisit_OnSamuraiWithFuncDeclNode_ShouldSplitTheName(t *testing.T) {
+	assert.Fail(t, "unimplemented test")
+}
+
+// TODO: add tests for interfaces, imports and comments
