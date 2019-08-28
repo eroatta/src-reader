9cc5d7d8ba6ff9120f4bebf2775c5f2104807975
eroatta <emilianoroatta@gmail.com>
2019-08-28 08:39:46 -0300

    added tests for successfully applied miners during mining step

--- step/mine_test.go
+++ step/mine_test.go
@@ -2,6 +2,8 @@ package step_test
 
 import (
 	"go/ast"
+	"go/parser"
+	"go/token"
 	"testing"
 
 	"github.com/eroatta/src-reader/code"
@@ -10,11 +12,11 @@ import (
 )
 
 func TestMine_OnNoFiles_ShouldReturnMinersWithoutResults(t *testing.T) {
-	processed := step.Mine([]code.File{}, miner{name: "empty"})
+	processed := step.Mine([]code.File{}, &miner{name: "empty"})
 
 	assert.Equal(t, 1, len(processed))
 
-	emptyMiner, ok := processed["empty"].(miner)
+	emptyMiner, ok := processed["empty"].(*miner)
 	assert.True(t, ok)
 	assert.NotNil(t, emptyMiner)
 	assert.Equal(t, 0, emptyMiner.visits)
@@ -27,18 +29,69 @@ func TestMine_OnEmptyMiners_ShouldReturnNoResults(t *testing.T) {
 }
 
 func TestMine_OnFileWithNilAST_ShouldReturnMinersWithoutResults(t *testing.T) {
-	processed := step.Mine([]code.File{{Name: "main.go"}}, miner{name: "empty"})
+	processed := step.Mine([]code.File{{Name: "main.go"}}, &miner{name: "empty"})
 
 	assert.Equal(t, 1, len(processed))
 
-	emptyMiner, ok := processed["empty"].(miner)
+	emptyMiner, ok := processed["empty"].(*miner)
 	assert.True(t, ok)
 	assert.NotNil(t, emptyMiner)
 	assert.Equal(t, 0, emptyMiner.visits)
 }
 
-func TestMine_OnTwoMiners_ShouldReturnResultsForEveryMiner(t *testing.T) {
-	assert.Fail(t, "not yet implemented")
+func TestMine_OnTwoMiners_ShouldReturnResultsBothMiners(t *testing.T) {
+	/* Created AST:
+	    0  *ast.File {
+	    1  .  Doc: nil
+	    2  .  Package: 1:1
+	    3  .  Name: *ast.Ident {
+	    4  .  .  NamePos: 1:9
+	    5  .  .  Name: "main"
+	    6  .  .  Obj: nil
+	    7  .  }
+	    8  .  Decls: nil
+	    9  .  Scope: *ast.Scope {
+	   10  .  .  Outer: nil
+	   11  .  .  Objects: map[string]*ast.Object (len = 0) {}
+	   12  .  }
+	   13  .  Imports: nil
+	   14  .  Unresolved: nil
+	   15  .  Comments: nil
+	   16  }
+	*/
+
+	testFileset := token.NewFileSet()
+
+	ast1, _ := parser.ParseFile(testFileset, "main.go", `package main`, parser.AllErrors)
+	file1 := code.File{
+		Name:    "main.go",
+		AST:     ast1,
+		FileSet: testFileset,
+	}
+
+	ast2, _ := parser.ParseFile(testFileset, "test.go", `package test`, parser.AllErrors)
+	file2 := code.File{
+		Name:    "test.go",
+		AST:     ast2,
+		FileSet: testFileset,
+	}
+
+	first := &miner{name: "first"}
+	second := &miner{name: "second"}
+
+	processed := step.Mine([]code.File{file1, file2}, first, second)
+
+	assert.Equal(t, 2, len(processed))
+
+	firstMiner, ok := processed["first"].(*miner)
+	assert.True(t, ok)
+	assert.NotNil(t, firstMiner)
+	assert.Equal(t, 8, firstMiner.visits)
+
+	secondMiner, ok := processed["second"].(*miner)
+	assert.True(t, ok)
+	assert.NotNil(t, secondMiner)
+	assert.Equal(t, 8, secondMiner.visits)
 }
 
 type miner struct {
@@ -46,11 +99,11 @@ type miner struct {
 	visits int
 }
 
-func (m miner) Name() string {
+func (m *miner) Name() string {
 	return m.name
 }
 
-func (m miner) Visit(n ast.Node) ast.Visitor {
+func (m *miner) Visit(n ast.Node) ast.Visitor {
 	m.visits++
 	return m
 }
