60c72f8541860a4379c231a9ddb9d384827357d9
eroatta <emilianoroatta@gmail.com>
2019-09-01 10:26:23 -0300

    added store step steps

--- step/store.go
+++ step/store.go
@@ -1,23 +1,29 @@
 package step
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/eroatta/src-reader/code"
 )
 
-// Storer TODO
+// Storer interface is used to define a custom storer.
 type Storer interface {
 	Save(ident code.Identifier) error
 }
 
-// Store receives a channel of Identifier and stores each element on the given storer.
-func Store(identc <-chan code.Identifier, str Storer) []code.Identifier {
+// Store receives a channel of code.Identifier and stores each element using the storer.
+// Unsaved identifiers are returned.
+func Store(identc <-chan code.Identifier, storer Storer) []code.Identifier {
 	failedIdent := make([]code.Identifier, 0)
-	for i := range identc {
-		err := str.Save(i)
+	for ident := range identc {
+		err := storer.Save(ident)
 		if err != nil {
-			// log.Println(err)
-			i.Error = err
-			failedIdent = append(failedIdent, i)
+			log.Println(fmt.Sprintf("Error saving identifier %s, from file %s and position %v",
+				ident.Name, ident.File, ident.Position))
+
+			ident.Error = err
+			failedIdent = append(failedIdent, ident)
 		}
 	}
 
