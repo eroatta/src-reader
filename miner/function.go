b2c25bc9e69236d6d3fdb477ee4da888f8862d41
eroatta <emilianoroatta@gmail.com>
2019-09-08 11:46:31 -0300

    added var decl mining logic for ex-function miner

--- miner/function.go
+++ miner/function.go
@@ -3,6 +3,7 @@ package miner
 import (
 	"fmt"
 	"go/ast"
+	"go/token"
 	"strings"
 
 	"github.com/eroatta/nounphrases"
@@ -11,16 +12,18 @@ import (
 
 // Text contains the mined text extracted from a function.
 type Text struct {
-	ID      string
-	Words   map[string]struct{}
-	Phrases map[string]struct{}
+	ID       string
+	DeclType token.Token
+	Words    map[string]struct{}
+	Phrases  map[string]struct{}
 }
 
-func newText(ID string) Text {
+func newText(ID string, declType token.Token) Text {
 	return Text{
-		ID:      ID,
-		Words:   make(map[string]struct{}),
-		Phrases: make(map[string]struct{}),
+		ID:       ID,
+		DeclType: declType,
+		Words:    make(map[string]struct{}),
+		Phrases:  make(map[string]struct{}),
 	}
 }
 
@@ -30,6 +33,7 @@ type Function struct {
 	dict        lists.List
 	pkg         string
 	pkgComments []*ast.CommentGroup
+	decls       []ast.Decl
 	functions   map[string]Text
 }
 
@@ -58,13 +62,14 @@ func (m Function) Visit(node ast.Node) ast.Visitor {
 	switch elem := node.(type) {
 	case *ast.File:
 		m.pkg = elem.Name.String()
+		m.decls = elem.Decls
 
 		// collect inner comment to use on their proper function
 		m.pkgComments = append(m.pkgComments, elem.Comments...)
 
 	case *ast.FuncDecl:
 		name := elem.Name.String()
-		functionText := newText(createFunctionID(m.pkg, name))
+		functionText := newText(createFunctionID(m.pkg, token.FUNC, name), token.FUNC)
 
 		if m.dict.Contains(name) {
 			functionText.Words[strings.ToLower(name)] = struct{}{}
@@ -86,6 +91,91 @@ func (m Function) Visit(node ast.Node) ast.Visitor {
 		}
 
 		m.functions[functionText.ID] = functionText
+
+	case *ast.GenDecl:
+		// TODO improve this code
+		if elem.Tok != token.VAR {
+			return m
+		}
+
+		var weCare bool
+		for _, d := range m.decls {
+			if d.Pos() == elem.Pos() {
+				weCare = true
+				break
+			}
+		}
+
+		if !weCare {
+			return m
+		}
+
+		// names (in case of multiple specs)
+		var decls []Text
+		var start int // TODO review
+		for _, spec := range elem.Specs {
+			if valSpec, ok := spec.(*ast.ValueSpec); ok {
+				for _, name := range valSpec.Names {
+					if name.Name == "_" {
+						continue
+					}
+
+					declText := newText(createFunctionID(m.pkg, token.VAR, name.String()), token.VAR)
+					// TODO add name if valid
+					if word := strings.ToLower(name.String()); m.dict.Contains(word) {
+						declText.Words[word] = struct{}{}
+					}
+
+					decls = append(decls, declText)
+					start++
+				}
+
+				fmt.Println(start)
+				for i, value := range valSpec.Values {
+					if val, ok := value.(*ast.BasicLit); ok && val.Kind == token.STRING {
+						// extract text from value
+						valStr := strings.Replace(val.Value, "\"", "", -1)
+						// TODO delete commas
+						for _, word := range strings.Split(valStr, " ") {
+							word = cleaner.ReplaceAllString(word, "")
+							if m.dict.Contains(strings.ToLower(word)) {
+								fmt.Println(fmt.Sprintf("Start %d, i %d, word: %s", start, i, word))
+								fmt.Println(len(decls))
+								decls[start+i-1].Words[strings.ToLower(word)] = struct{}{}
+							}
+						}
+
+						// get phrases
+						phrases, _ := nounphrases.Find(cleanComment(valStr))
+						for _, phr := range phrases {
+							decls[start+i-1].Phrases[phr] = struct{}{} // TODO: review index
+						}
+					}
+				}
+			}
+		}
+
+		// decl doc
+		dummyCommentText := newText("", token.VAR)
+		if elem.Doc != nil {
+			for _, comment := range elem.Doc.List {
+				dummyCommentText = extractWordAndPhrasesFromComment(dummyCommentText, comment.Text, m.dict)
+			}
+		}
+
+		// merge decl doc
+		for i := range decls {
+			for k, v := range dummyCommentText.Words {
+				decls[i].Words[k] = v
+			}
+
+			for k, v := range dummyCommentText.Phrases {
+				decls[i].Phrases[k] = v
+			}
+
+			// add them to the map
+			m.functions[decls[i].ID] = decls[i]
+		}
 	}
 
 	return m
@@ -117,8 +207,8 @@ func extractWordAndPhrasesFromComment(functionText Text, comment string, list li
 	return functionText
 }
 
-func createFunctionID(pkg string, name string) string {
-	return fmt.Sprintf("%s+%s", pkg, name)
+func createFunctionID(pkg string, declType token.Token, name string) string {
+	return fmt.Sprintf("%s++%s::%s", pkg, declType, name)
 }
 
 // FunctionsText returns a map of function names and the mined text for each function.
--- miner/function_test.go
+++ miner/function_test.go
@@ -59,16 +59,18 @@ func TestVisit_OnFunction_ShouldReturnFunctionsWordsAndPhrases(t *testing.T) {
 		expected map[string]miner.Text
 	}{
 		{"no_functions", "package main", make(map[string]miner.Text)},
-		{"without_text_or_comments", srcWithoutTextOrComments, map[string]miner.Text{
-			"main+main": miner.Text{
-				ID:      "main+main",
-				Words:   map[string]struct{}{"main": struct{}{}},
-				Phrases: make(map[string]struct{}),
+		{"functions_without_text_or_comments", srcWithoutTextOrComments, map[string]miner.Text{
+			"main++func::main": miner.Text{
+				ID:       "main++func::main",
+				DeclType: token.FUNC,
+				Words:    map[string]struct{}{"main": struct{}{}},
+				Phrases:  make(map[string]struct{}),
 			},
 		}},
-		{"with_text_and_comments", srcWithTextAndComments, map[string]miner.Text{
-			"main+main": miner.Text{
-				ID: "main+main",
+		{"functions_with_text_and_comments", srcWithTextAndComments, map[string]miner.Text{
+			"main++func::main": miner.Text{
+				ID:       "main++func::main",
+				DeclType: token.FUNC,
 				Words: map[string]struct{}{
 					"main":     struct{}{},
 					"is":       struct{}{},
@@ -86,14 +88,16 @@ func TestVisit_OnFunction_ShouldReturnFunctionsWordsAndPhrases(t *testing.T) {
 				},
 			},
 		}},
-		{"with_multiple_functions", srcWithMultipleFunctions, map[string]miner.Text{
-			"main+main": miner.Text{
-				ID:      "main+main",
-				Words:   map[string]struct{}{"main": struct{}{}},
-				Phrases: make(map[string]struct{}),
+		{"functions_with_multiple_functions", srcWithMultipleFunctions, map[string]miner.Text{
+			"main++func::main": miner.Text{
+				ID:       "main++func::main",
+				DeclType: token.FUNC,
+				Words:    map[string]struct{}{"main": struct{}{}},
+				Phrases:  make(map[string]struct{}),
 			},
-			"main+another": miner.Text{
-				ID: "main+another",
+			"main++func::another": miner.Text{
+				ID:       "main++func::another",
+				DeclType: token.FUNC,
 				Words: map[string]struct{}{
 					"another":  struct{}{},
 					"function": struct{}{},
@@ -119,7 +123,159 @@ func TestVisit_OnFunction_ShouldReturnFunctionsWordsAndPhrases(t *testing.T) {
 	}
 }
 
-func TestVisit_OnFunctionWithRealLifeFile_ShouldReturnFunctionsWordsAndPhrases(t *testing.T) {
+func TestVisit_OnFunctionWithVarDecl_ShouldReturnWordsAndPhrases(t *testing.T) {
+	srcVarWithoutComments := `
+		package main
+		
+		var Common string
+	`
+
+	srcVarWithDocComments := `
+		package main
+
+		// outer comment
+		var Common string
+	`
+	srcVarWithDocAndLineComments := `
+		package main
+
+		// outer comment
+		var Common string // inner comment
+	`
+
+	srcMultipleVarSpecs := `
+		package main
+
+		// outer comment
+		var common,regular string
+	`
+
+	srcVarBlock := `
+		package main
+
+		// outer comment
+		var (
+			common string
+			regular string = "valid"
+			nrXX int = 32
+		)
+	`
+
+	tests := []struct {
+		name     string
+		src      string
+		expected map[string]miner.Text
+	}{
+		{"variable_without_comments", srcVarWithoutComments, map[string]miner.Text{
+			"main++var::Common": miner.Text{
+				ID:       "main++var::Common",
+				DeclType: token.VAR,
+				Words:    map[string]struct{}{"common": struct{}{}},
+				Phrases:  make(map[string]struct{}),
+			}}},
+		{"variable_with_doc_comments", srcVarWithDocComments, map[string]miner.Text{
+			"main++var::Common": miner.Text{
+				ID:       "main++var::Common",
+				DeclType: token.VAR,
+				Words: map[string]struct{}{
+					"comment": struct{}{},
+					"common":  struct{}{},
+					"outer":   struct{}{},
+				},
+				Phrases: map[string]struct{}{
+					"outer comment": struct{}{},
+				},
+			}}},
+		{"variable_with_doc_and_ignored_line_comments", srcVarWithDocAndLineComments, map[string]miner.Text{
+			"main++var::Common": miner.Text{
+				ID:       "main++var::Common",
+				DeclType: token.VAR,
+				Words: map[string]struct{}{
+					"comment": struct{}{},
+					"common":  struct{}{},
+					"outer":   struct{}{},
+				},
+				Phrases: map[string]struct{}{
+					"outer comment": struct{}{},
+				},
+			}}},
+		{"multiple_variables_same_line", srcMultipleVarSpecs, map[string]miner.Text{
+			"main++var::common": miner.Text{
+				ID:       "main++var::common",
+				DeclType: token.VAR,
+				Words: map[string]struct{}{
+					"comment": struct{}{},
+					"common":  struct{}{},
+					"outer":   struct{}{},
+				},
+				Phrases: map[string]struct{}{
+					"outer comment": struct{}{},
+				}},
+			"main++var::regular": miner.Text{
+				ID:       "main++var::regular",
+				DeclType: token.VAR,
+				Words: map[string]struct{}{
+					"comment": struct{}{},
+					"regular": struct{}{},
+					"outer":   struct{}{},
+				},
+				Phrases: map[string]struct{}{
+					"outer comment": struct{}{},
+				},
+			}}},
+		{"var_block", srcVarBlock, map[string]miner.Text{
+			"main++var::common": miner.Text{
+				ID:       "main++var::common",
+				DeclType: token.VAR,
+				Words: map[string]struct{}{
+					"comment": struct{}{},
+					"common":  struct{}{},
+					"outer":   struct{}{},
+				},
+				Phrases: map[string]struct{}{
+					"outer comment": struct{}{},
+				}},
+			"main++var::regular": miner.Text{
+				ID:       "main++var::regular",
+				DeclType: token.VAR,
+				Words: map[string]struct{}{
+					"comment": struct{}{},
+					"regular": struct{}{},
+					"outer":   struct{}{},
+					"valid":   struct{}{},
+				},
+				Phrases: map[string]struct{}{
+					"outer comment": struct{}{},
+				}},
+			"main++var::nrXX": miner.Text{
+				ID:       "main++var::nrXX",
+				DeclType: token.VAR,
+				Words: map[string]struct{}{
+					"comment": struct{}{},
+					"outer":   struct{}{},
+				},
+				Phrases: map[string]struct{}{
+					"outer comment": struct{}{},
+				},
+			}}},
+	}
+
+	for _, fixture := range tests {
+		t.Run(fixture.name, func(t *testing.T) {
+			fs := token.NewFileSet()
+			node, _ := parser.ParseFile(fs, "", []byte(fixture.src), parser.ParseComments)
+
+			function := miner.NewFunction(lists.Dicctionary)
+			ast.Walk(function, node)
+
+			functions := function.FunctionsText()
+			assert.Equal(t, len(fixture.expected), len(functions))
+			assert.Equal(t, fixture.expected, functions)
+		})
+	}
+}
+
+/*func TestVisit_OnFunctionWithRealLifeFile_ShouldReturnFunctionsWordsAndPhrases(t *testing.T) {
 	src := `
 	`
 	fs := token.NewFileSet()
@@ -131,4 +287,4 @@ func TestVisit_OnFunctionWithRealLifeFile_ShouldReturnFunctionsWordsAndPhrases(t
 	functions := function.FunctionsText()
 	assert.Equal(t, 3, len(functions))
 	// assert.Equal(t, fixture.expected, functions)
-}
+}*/
