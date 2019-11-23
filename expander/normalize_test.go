6232e8549cc822281ccdf0bff5428442cb33c34e
eroatta <emilianoroatta@gmail.com>
2019-11-23 16:13:51 -0300

    added Normalize expander for expansion step

--- expander/normalize_test.go
+++ expander/normalize_test.go
@@ -1,29 +1,41 @@
 package expander_test
 
 import (
+	"go/token"
 	"testing"
 
 	"github.com/eroatta/src-reader/code"
 	"github.com/eroatta/src-reader/expander"
+	"github.com/eroatta/src-reader/miner"
+	"github.com/eroatta/token/expansion"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewNormalize_ShouldReturnNormalize(t *testing.T) {
-	normalize := expander.NewNormalize()
+	var simCalculatorMock similarityCalculatorMock
+	var decls map[string]miner.Decl
+	var expansions expansion.Set
+	normalize := expander.NewNormalize(simCalculatorMock, decls, expansions)
 
 	assert.NotNil(t, normalize)
 	assert.Equal(t, "normalize", normalize.Name())
 }
 
 func TestApplicableOn_OnNormalize_ShouldReturnGenTest(t *testing.T) {
-	normalize := expander.NewNormalize()
+	var simCalculatorMock similarityCalculatorMock
+	var decls map[string]miner.Decl
+	var expansions expansion.Set
+	normalize := expander.NewNormalize(simCalculatorMock, decls, expansions)
 
 	assert.NotNil(t, normalize)
 	assert.Equal(t, "gentest", normalize.ApplicableOn())
 }
 
 func TestExpand_OnNormalizeWhenNoSplitsApplicable_ShouldReturnEmptyResults(t *testing.T) {
-	normalize := expander.NewNormalize()
+	var simCalculatorMock similarityCalculatorMock
+	var decls map[string]miner.Decl
+	var expansions expansion.Set
+	normalize := expander.NewNormalize(simCalculatorMock, decls, expansions)
 
 	ident := code.Identifier{
 		Name: "str",
@@ -38,8 +50,44 @@ func TestExpand_OnNormalizeWhenNoSplitsApplicable_ShouldReturnEmptyResults(t *te
 	assert.EqualValues(t, []string{}, got)
 }
 
+func TestExpand_OnNormalizeWhenNoDeclFound_ShouldReturnUnexpandedResults(t *testing.T) {
+	var simCalculatorMock similarityCalculatorMock
+	var decls map[string]miner.Decl
+	var expansions expansion.Set
+	normalize := expander.NewNormalize(simCalculatorMock, decls, expansions)
+
+	ident := code.Identifier{
+		Name: "str",
+		Splits: map[string][]string{
+			"gentest": []string{"str"},
+		},
+	}
+
+	got := normalize.Expand(ident)
+
+	assert.Equal(t, 1, len(got))
+	assert.EqualValues(t, []string{"str"}, got)
+}
+
 func TestExpand_OnNormalize_ShouldReturnExpansions(t *testing.T) {
-	normalize := expander.NewNormalize()
+	simCalculatorMock := similarityCalculatorMock{
+		"string-builder": 1.0,
+	}
+
+	decls := map[string]miner.Decl{
+		"str": miner.Decl{
+			ID:       "str",
+			DeclType: token.FUNC,
+			Words: map[string]struct{}{
+				"builder": struct{}{},
+			},
+			Phrases: map[string]struct{}{},
+		},
+	}
+
+	expansions := expansion.NewSetBuilder().AddStrings("string").Build()
+
+	normalize := expander.NewNormalize(simCalculatorMock, decls, expansions)
 
 	ident := code.Identifier{
 		Name: "str",
@@ -52,4 +100,17 @@ func TestExpand_OnNormalize_ShouldReturnExpansions(t *testing.T) {
 
 	assert.Equal(t, 1, len(got))
 	assert.EqualValues(t, []string{"string"}, got)
+}
+
+type similarityCalculatorMock map[string]float64
+
+func (s similarityCalculatorMock) Similarity(word string, another string) float64 {
+	var key string
+	if word < another {
+		key = word + "-" + another
+	} else {
+		key = another + "-" + word
+	}
+
+	return s[key]
 }
