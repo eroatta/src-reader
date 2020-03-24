44b8e7f396d7e8e33e5f83cd1694189c9c8afd2b
eroatta <emilianoroatta@gmail.com>
2020-03-24 16:45:10 -0300

    added in memory project repository

---
+++ adapter/persistence/in_memory.go
@@ -0,0 +1,32 @@
+package persistence
+
+import (
+	"context"
+
+	"github.com/eroatta/src-reader/entity"
+	"github.com/eroatta/src-reader/repository"
+)
+
+func NewInMemoryProjectRepository() *InMemoryProjectRepository {
+	return &InMemoryProjectRepository{
+		repos: make(map[string]*entity.Project),
+	}
+}
+
+type InMemoryProjectRepository struct {
+	repos map[string]*entity.Project
+}
+
+func (r InMemoryProjectRepository) Add(ctx context.Context, project entity.Project) error {
+	r.repos[project.URL] = &project
+	return nil
+}
+
+func (r InMemoryProjectRepository) GetByURL(ctx context.Context, url string) (entity.Project, error) {
+	project, ok := r.repos[url]
+	if !ok {
+		return entity.Project{}, repository.ErrNoResults
+	}
+
+	return *project, nil
+}
