c3a0e703b26a5fb42ef3dd86b5f5a0eb83d80b00
eroatta <emilianoroatta@gmail.com>
2020-03-25 18:30:45 -0300

    setting up tests for github metadata repository

--- adapter/github/metadata_repository.go
+++ adapter/github/metadata_repository.go
@@ -1,15 +1,26 @@
 package github
 
-import "net/http"
+import (
+	"context"
+	"net/http"
 
-func NewRESTMetadataRepository(httpClient *http.Client, accessToken string) *RESTMetadataRepository {
+	"github.com/eroatta/src-reader/entity"
+)
+
+func NewRESTMetadataRepository(httpClient *http.Client, baseURL string, accessToken string) *RESTMetadataRepository {
 	return &RESTMetadataRepository{
 		httpClient:  httpClient,
+		baseURL:     baseURL,
 		accessToken: accessToken,
 	}
 }
 
 type RESTMetadataRepository struct {
 	httpClient  *http.Client
+	baseURL     string
 	accessToken string
+}
+
+func (r RESTMetadataRepository) RetrieveMetadata(ctx context.Context, remoteRepository string) (entity.Metadata, error) {
+	return entity.Metadata{}, nil
 }
--- adapter/github/metadata_repository_test.go
+++ adapter/github/metadata_repository_test.go
@@ -1,7 +1,10 @@
 package github
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,15 +12,25 @@ import (
 
 func TestNewRESTMetadataRepository_ShouldReturnNewInstance(t *testing.T) {
 	httpClient := &http.Client{}
-	repository := NewRESTMetadataRepository(httpClient, "token")
+	repository := NewRESTMetadataRepository(httpClient, "baseURL", "accessToken")
 
 	assert.NotNil(t, repository)
 	assert.Equal(t, httpClient, repository.httpClient)
-	assert.Equal(t, "token", repository.accessToken)
+	assert.Equal(t, "baseURL", repository.baseURL)
+	assert.Equal(t, "accessToken", repository.accessToken)
 }
 
 func TestRetrieveMetadata_OnRESTMetadataRepository_WhileInvalidToken_ShouldReturnError(t *testing.T) {
-	assert.FailNow(t, "not yet implemented")
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hola!")
+	}))
+	defer server.Close()
+
+	repository := NewRESTMetadataRepository(server.Client(), server.URL, "token")
+
+	_, err := repository.RetrieveMetadata(context.TODO(), "owner/reponame")
+
+	assert.Error(t, err)
 }
 
 func TestRetrieveMetadata_OnRESTMetadataRepository_WhileNotFoundGitHubProject_ShouldReturnError(t *testing.T) {
