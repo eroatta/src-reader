975853d73f5f74f18522be79d98d32940796c784
eroatta <emilianoroatta@gmail.com>
2020-04-20 19:42:36 -0300

    added godoc for splitting/expansion algorithm factories

--- entity/algorithm.go
+++ entity/algorithm.go
@@ -40,6 +40,18 @@ type Splitter interface {
 	Split(token string) []Split
 }
 
+// SplitterAbstractFactory is an interface for creating splitting algorithm factories.
+type SplitterAbstractFactory interface {
+	// Get returns a SplitterFactory for the selectd splitting algorithm.
+	Get(algorithm string) (SplitterFactory, error)
+}
+
+// SplitterFactory is an interface for creating splitting algorithm instances.
+type SplitterFactory interface {
+	// Make returns a splitting algorithm instance built from miners.
+	Make(miners map[MinerType]Miner) (Splitter, error)
+}
+
 // Expander interface is used to define a custom expander.
 type Expander interface {
 	// Name returns the name of the custom expander.
@@ -50,18 +62,14 @@ type Expander interface {
 	Expand(ident Identifier) []Expansion
 }
 
-type SplitterAbstractFactory interface {
-	Get(algorithm string) (SplitterFactory, error)
-}
-
-type SplitterFactory interface {
-	Make(miningResults map[MinerType]Miner) (Splitter, error)
-}
-
+// ExpanderAbstractFactory is an interface for creating expandion algorithm factories.
 type ExpanderAbstractFactory interface {
+	// Get returns a ExpanderFactory for the selectd expansion algorithm.
 	Get(algorithm string) (ExpanderFactory, error)
 }
 
+// ExpanderFactory is an interface for creating expansion algorithm instances.
 type ExpanderFactory interface {
+	// Make returns an expansion algorithm instance built from miners.
 	Make(miningResults map[MinerType]Miner) (Expander, error)
 }
