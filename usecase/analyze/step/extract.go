045e1a4e567e0ac95ec8515aa363f24cd636b54f
eroatta <emilianoroatta@gmail.com>
2020-05-03 15:14:27 -0300

    fixed godoc

--- usecase/analyze/step/extract.go
+++ usecase/analyze/step/extract.go
@@ -6,7 +6,7 @@ import (
 	"github.com/eroatta/src-reader/entity"
 )
 
-// extract traverses each Abstract Syntax Tree and applies an extractor
+// Extract traverses each Abstract Syntax Tree and applies an extractor
 // to retrieve the identifiers that are interest of us.
 func Extract(files []entity.File, factory entity.ExtractorFactory) chan entity.Identifier {
 	identc := make(chan entity.Identifier)
