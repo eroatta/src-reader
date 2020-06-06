02f08e1c09c7db1c568b0d94947f4e5130ea8249
eroatta <emilianoroatta@gmail.com>
2020-06-06 18:18:02 -0300

    removed duplicated code

--- port/incoming/adapter/rest/file_handler.go
+++ port/incoming/adapter/rest/file_handler.go
@@ -1,6 +1,7 @@
 package rest
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"strings"
@@ -11,39 +12,25 @@ import (
 
 func RegisterOriginalFileUsecase(r *gin.Engine, uc file.OriginalFileUsecase) *gin.Engine {
 	r.GET("/files/originals/:owner/:project/*file", func(c *gin.Context) {
-		getOriginalFile(c, uc)
+		getFile(c, uc)
 	})
 
 	return r
-}
-
-func getOriginalFile(ctx *gin.Context, uc file.OriginalFileUsecase) {
-	projectRef := fmt.Sprintf("%s/%s", ctx.Param("owner"), ctx.Param("project"))
-	raw, err := uc.Process(ctx, projectRef, strings.TrimPrefix(ctx.Param("file"), "/"))
-	switch err {
-	case nil:
-		// do nothing
-	case file.ErrProjectNotFound:
-		ctx.AbortWithStatus(http.StatusNotFound)
-	case file.ErrFileNotFound:
-		ctx.AbortWithStatus(http.StatusNotFound)
-	default:
-		ctx.String(http.StatusInternalServerError, "Ooops! Something went wrong...")
-		return
-	}
-
-	ctx.String(200, string(raw))
 }
 
 func RegisterRewrittenFileUsecase(r *gin.Engine, uc file.RewrittenFileUsecase) *gin.Engine {
 	r.GET("/files/rewritten/:owner/:project/*file", func(c *gin.Context) {
-		getRewrittenFile(c, uc)
+		getFile(c, uc)
 	})
 
 	return r
 }
 
-func getRewrittenFile(ctx *gin.Context, uc file.OriginalFileUsecase) {
+type fileUsecase interface {
+	Process(ctx context.Context, projectRef string, filename string) ([]byte, error)
+}
+
+func getFile(ctx *gin.Context, uc fileUsecase) {
 	projectRef := fmt.Sprintf("%s/%s", ctx.Param("owner"), ctx.Param("project"))
 	raw, err := uc.Process(ctx, projectRef, strings.TrimPrefix(ctx.Param("file"), "/"))
 	switch err {
